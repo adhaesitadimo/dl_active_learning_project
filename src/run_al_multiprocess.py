f9ed724e512d2268e15ea9bbf5a553e4e330b9fb
adhaesitadimo <faktorrus@gmail.com>
2020-06-07 15:45:01 +0000

    auto models

--- src/run_al_multiprocess.py
+++ src/run_al_multiprocess.py
@@ -1,52 +1,50 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+import itertools
+import os
+import multiprocessing
+from pathlib import Path
+
+import time
 import flair
-from flair.embeddings import WordEmbeddings, ELMoEmbeddings, StackedEmbeddings, FlairEmbeddings
+import hydra
+import nltk
+import numpy as np
+import pandas as pd
+import torch
+
+from actleto import ActiveLearner, make_libact_strategy_ctor
+
+from al4ner.auto_sequence_tagger import SequenceTaggerAuto, AutoModelForTokenClassificationCustom, ModelTrainerAuto
+from al4ner.auto_sequence_tagger.auto_utils import get_parameters_without_decay
+from al4ner.auto_sequence_tagger.metrics import f1_entity_level
+
+from flair.data import Dictionary
+from flair.datasets import ColumnCorpus, ColumnDataset
+from flair.embeddings import ELMoEmbeddings, StackedEmbeddings, WordEmbeddings, FlairEmbeddings
 from flair.models import SequenceTagger
 from flair.trainers import ModelTrainer
 from flair.training_utils import EvaluationMetric
-from flair.data import Dictionary
-
-import time
-import hydra
-import pandas as pd
-import numpy as np
-import itertools
-import multiprocessing
-import os
-import torch
-from seqeval.metrics import f1_score
-from pathlib import Path
-import nltk
-
-from al4ner.libact_flair import LibActFlair, PositiveLessCertain, LibActFlairBayes
-from al4ner.libact_crf import LibActCrf
-from al4ner.libact_nn import LibActNN
-
-from libact.query_strategies import UncertaintySampling, RandomSampling
+from libact.query_strategies import RandomSampling, UncertaintySampling
 from al4ner.libact_nn import RandomSamplingWithRetraining
-from actleto import ActiveLearner, make_libact_strategy_ctor
-
-from vadim_ml.memoize import memoize
-from vadim_ml.io import load_file, dump_file
-
-from flair.datasets import ColumnCorpus, ColumnDataset
-from torch import nn
-
-from al4ner.mc_dropout import convert_to_mc_dropout, DropoutMC
-from bert_sequence_tagger import SequenceTaggerBert, BertForTokenClassificationCustom, ModelTrainerBert
-from bert_sequence_tagger.bert_utils import get_parameters_without_decay
-from bert_sequence_tagger.metrics import f1_entity_level
-
-from pytorch_transformers import BertTokenizer, AdamW, WarmupLinearSchedule, WarmupConstantSchedule
+from pytorch_transformers import (AdamW, BertTokenizer, WarmupConstantSchedule,
+                                  WarmupLinearSchedule)
+from seqeval.metrics import f1_score
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
+from al4ner.libact_crf import LibActCrf
+from al4ner.libact_flair import LibActFlair, PositiveLessCertain, LibActFlairBayes
+from al4ner.libact_nn import LibActNN, LibActNNBayes
+from al4ner.mc_dropout import convert_to_mc_dropout
+
+from transformers import AutoTokenizer, AutoModel
+
+from vadim_ml.io import dump_file, load_file
+from vadim_ml.memoize import memoize
+
 #MIN_LEARNING_RATE = LEARNING_RATE / (2**4)
-
-BERT_LEARNING_RATE = 5e-5
-VALIDATION_RATIO = 0.25
-ANNEAL_FACTOR = 0.5
-PATIENCE = 2
-MAX_TO_ANNEAL = 3
-
+#MAX_TO_ANNEAL = 3
 
 def load_task(data_folder, task, tag_column, preprocess):
     X = {'train': [], 'test': []}
@@ -101,7 +99,8 @@ def strategies_to_try(tp):
         raise ValueError('Wrong strategy')
 
 
-def y_train2y_seed(y_train, n_seeds_per_class=100):
+def y_train2y_seed(y_train, n_seeds_per_class=100, rpt=0):
+    np.random.seed(rpt)
     y_vals = set(tag for row in y_train for tag in row)
 
     indexes = np.array(range(len(y_train)))
@@ -121,21 +120,15 @@ def y_train2y_seed(y_train, n_seeds_per_class=100):
 
     return Y_seed
 
-
-def y_train2y_seed_percent(y_train, percent=0.02):
+def y_train2y_seed_percent(y_train, percent=0.02, rpt=0):
+    np.random.seed(rpt)
     y_vals = set(tag for row in y_train for tag in row)
 
     indexes = np.array(range(len(y_train)))
     known_indexes = []
     selected_indices = []
-    
-    #for label in y_vals:
-    #    selected_indices += indexes[[label in y for y in y_train]]
-        
-    print(len(indexes))
-    print(int(len(y_train) * 0.02))
-        
-    known_indexes += list(np.random.choice(indexes, size=int(len(y_train) * 0.02), replace=False))
+            
+    known_indexes += list(np.random.choice(indexes, size=int(len(y_train) * percent), replace=False))
 
     known_indexes = list(set(known_indexes))
 
@@ -144,14 +137,11 @@ def y_train2y_seed_percent(y_train, percent=0.02):
     for i in known_indexes:
         tags_seed[i] = y_train[i]
 
-    Y_seed = tags_seed
+    return tags_seed
 
-    return Y_seed
-
-
-
-def emulate_active_learning(train_tags, active_learner,
-                            max_iterations=12,
+def emulate_active_learning(train_tags, 
+                            active_learner, 
+                            max_iterations, 
                             fit_model=False):
     print('Start emulating active learning.')
     active_learner.start()
@@ -191,34 +181,36 @@ def get_embeddings(emb_name):
         raise ValueError('Wrong embedding type')
 
 
-def create_libact_adaptor_bert(tag2index, index2tag, adaptor_type, *args, **kwargs):
+def create_libact_adaptor(tag2index, index2tag, adaptor_type, *args, **kwargs):
     def model_ctor():
-        bert_tokenizer = BertTokenizer.from_pretrained('bert-base-cased',
+        auto_tokenizer = AutoTokenizer.from_pretrained(kwargs['config'].model.tokenizer,
                                                        cache_dir=kwargs['config'].model.cache_dir,
                                                        do_lower_case=False)
         # BIO_BERT
-        model = BertForTokenClassificationCustom.from_pretrained('bert-base-cased',
+        model = AutoModelForTokenClassificationCustom.from_pretrained(kwargs['config'].model.classifier,
                                                                  cache_dir=kwargs['config'].model.cache_dir,
                                                                  num_labels=len(tag2index)).cuda()
 
-        if config.model.bayes:
-            if config.model.bayes_type == 'mcd_last_layer':
+        if kwargs['config'].model.bayes:
+            if kwargs['config'].model.bayes_type == 'mcd_last_layer':
                 # last dropout layer name in BertForTokenClassification
                 model.dropout = DropoutMC(p=model.dropout.p, activate=True)
-            elif config.model.bayes_type == 'mcd_all_layers':
+            elif kwargs['config'].model.bayes_type == 'mcd_all_layers':
                 convert_to_mc_dropout(model)
 
-        seq_tagger = SequenceTaggerBert(model, bert_tokenizer, idx2tag=index2tag, tag2idx=tag2index)
+        seq_tagger = SequenceTaggerAuto(model, auto_tokenizer, idx2tag=index2tag, tag2idx=tag2index)
         return seq_tagger
 
     def trainer_ctor(seq_tagger, corpus_len, train_dataloader, val_dataloader):
-        optimizer = AdamW(get_parameters_without_decay(seq_tagger._bert_model),
-                          lr=BERT_LEARNING_RATE, betas=(0.9, 0.999),
+        optimizer = AdamW(get_parameters_without_decay(seq_tagger._auto_model),
+                          lr=kwargs['config'].model.lr, betas=(0.9, 0.999),
                           eps=1e-6, weight_decay=0.01, correct_bias=True)
+        
+        print('CORPUS LEN: ',corpus_len)
+        
+        lr_scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=kwargs['config'].model.anneal_factor, patience=kwargs['config'].model.patience)
 
-        lr_scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=ANNEAL_FACTOR, patience=PATIENCE)
-
-        trainer = ModelTrainerBert(seq_tagger,
+        trainer = ModelTrainerAuto(seq_tagger,
                                    optimizer,
                                    lr_scheduler,
                                    train_dataloader,
@@ -229,7 +221,7 @@ def create_libact_adaptor_bert(tag2index, index2tag, adaptor_type, *args, **kwar
                                    max_grad_norm=1.,
                                    validation_metrics=[f1_entity_level],
                                    decision_metric=lambda metrics: metrics[0],
-                                   smallest_lr=config.model.smallest_lr)
+                                   smallest_lr=kwargs['config'].model.smallest_lr)
         # BERT_LEARNING_RATE / (MAX_TO_ANNEAL**(1./ANNEAL_FACTOR) + 0.1)
 
         return trainer
@@ -241,7 +233,7 @@ def create_libact_adaptor_bert(tag2index, index2tag, adaptor_type, *args, **kwar
                         bs_pred=kwargs['config'].model.ebs,
                         train_from_scratch=True,
                         retrain_epochs=kwargs['config'].model.n_epochs,
-                        valid_ratio=VALIDATION_RATIO,
+                        valid_ratio=kwargs['config'].model.valid_ratio,
                         string_input=False)
 
 
@@ -258,13 +250,18 @@ def run_experiment(config, repeat=1):
     print('Done.')
 
     strat = strategies_to_try(config.al.strat_name)
-
     model_name = config.model.model_type
 
+    print(f'######################==Repeat {repeat} ==#####################')
 
+    strat = strategies_to_try(config.al.strat_name)
+
+    model_name = config.model.model_type
+        
     if config.al.percent:
+        percent = 0.02
         print('FULL:', len(y_train))
-        y_seed = y_train2y_seed_percent(y_train)
+        y_seed = y_train2y_seed_percent(y_train, percent, rpt=repeat)
         selector = [False for _ in range(len(y_seed))]
         for ind, answ in enumerate(y_seed):
             if answ is None:
@@ -276,20 +273,17 @@ def run_experiment(config, repeat=1):
 
         y_nonempty = np.array(y_seed)[selector]
         print('2PERCENT:', len(y_nonempty))
-        max_samples_number = int(len(y_seed) * 0.02)
-
+        max_samples_number = int(len(y_seed) * percent)
     else:
-        y_seed = y_train2y_seed(y_train)
+        y_seed = y_train2y_seed(y_train, rpt=repeat)
         max_samples_number = config.al.max_samples_number
 
-    print('MAX_SAMPLES:', max_samples_number)
 
     if 'flair' in config.model.model_type:
         print(config.model.model_type)
-
+            
         bayes_type = config.model.bayes_type if config.model.bayes else 'no_bayes'
         models_path = os.path.join(config.exp_path, f'{model_name}_{config.model.emb_name}_{bayes_type}/{config.al.strat_name}')
-        print(models_path)
         os.makedirs(models_path, exist_ok=True)
 
         if os.path.exists(os.path.join(models_path, f'statistics{repeat}.json')):
@@ -300,58 +294,68 @@ def run_experiment(config, repeat=1):
         emb = get_embeddings(config.model.emb_name)
 
         tagger = SequenceTagger(hidden_size=config.model.hidden_size,
-                                embeddings=emb(),
-                                tag_dictionary=tag_dictionary,
-                                tag_type=config.data.task,
-                                use_crf=True)
+                                    embeddings=emb(),
+                                    tag_dictionary=tag_dictionary,
+                                    tag_type=config.data.task,
+                                    use_crf=True)
         print(config.model.bayes)
         if config.model.bayes:
+            print('BAYES CHOSEN')
             convert_to_mc_dropout(tagger, (nn.Dropout, flair.nn.WordDropout, flair.nn.LockedDropout), option='flair')
             active_tagger = LibActFlairBayes(tagger,
-                                        base_path=models_path,
-                                        reset_model_before_train=True,
-                                        mini_batch_size=config.model.bs,
-                                        eval_mini_batch_size=config.model.ebs,
-                                        checkpoint=False,
-                                        learning_rate=config.model.lr,
-                                        index_subset=False,
-                                        save_all_models=False,
-                                        max_epochs=config.model.n_epochs,
-                                        min_learning_rate=config.model.min_lr)
-
-
+                                            base_path=models_path,
+                                            reset_model_before_train=True,
+                                            mini_batch_size=config.model.bs,
+                                            eval_mini_batch_size=config.model.ebs,
+                                            checkpoint=False,
+                                            learning_rate=config.model.lr,
+                                            index_subset=False,
+                                            save_all_models=False,
+                                            max_epochs=config.model.n_epochs,
+                                            min_learning_rate=config.model.min_lr)
+                
+            print(active_tagger)
+                
         else:
             active_tagger = LibActFlair(tagger,
-                                        base_path=models_path,
-                                        reset_model_before_train=True,
-                                        mini_batch_size=config.model.bs,
-                                        eval_mini_batch_size=config.model.ebs,
-                                        checkpoint=False,
-                                        learning_rate=config.model.lr,
-                                        index_subset=False,
-                                        save_all_models=False,
-                                        max_epochs=config.model.n_epochs,
-                                        min_learning_rate=config.model.min_lr)
+                                            base_path=models_path,
+                                            reset_model_before_train=True,
+                                            mini_batch_size=config.model.bs,
+                                            eval_mini_batch_size=config.model.ebs,
+                                            checkpoint=False,
+                                            learning_rate=config.model.lr,
+                                            index_subset=False,
+                                            save_all_models=False,
+                                            max_epochs=config.model.n_epochs,
+                                            min_learning_rate=config.model.min_lr)
         fit_model = False
 
     elif config.model.model_type == 'crf':
         models_path = os.path.join(config.exp_path, model_name)
-        print(models_path)
+        os.makedirs(models_path, exist_ok=True)
+
         if os.path.exists(os.path.join(models_path, f'statistics{repeat}.json')):
             print(f'statistics{repeat}.json already exists. Next')
             return
 
         active_tagger = LibActCrf(algorithm="lbfgs",
-                                  c1=0.1,
-                                  c2=0.1,
-                                  max_iterations=100,
-                                  all_possible_transitions=True)
+                                      c1=0.1,
+                                      c2=0.1,
+                                      max_iterations=100,
+                                      all_possible_transitions=True)
         fit_model = True
 
-    elif config.model.model_type == 'bert':
+    elif config.model.model_type == 'transformers':
 
-        bayes_type = config.model.bayes_type if config.model.bayes else 'no_bayes'
-        models_path = os.path.join(config.exp_path, f'{model_name}_{bayes_type}')
+        if config.model.bayes:
+            libactnn = LibActNNBayes
+            bayes_type = config.model.bayes_type
+        else:
+            libactnn = LibActNN
+            bayes_type = 'no_bayes'
+            
+        models_path=os.path.join(config.exp_path,f'{model_name}_{bayes_type}/{config.al.strat_name}')
+        print(models_path)
 
         if os.path.exists(os.path.join(models_path, f'statistics{repeat}.json')):
             print(f'statistics{repeat}.json already exists. Next')
@@ -359,26 +363,27 @@ def run_experiment(config, repeat=1):
 
         index2tag = ['[PAD]'] + tag_dictionary.get_items()
         tag2index = {e: i for i, e in enumerate(index2tag)}
-        active_tagger = create_libact_adaptor_bert(tag2index, index2tag, LibActNN, config=config)
+        active_tagger = create_libact_adaptor(tag2index, index2tag, LibActNN, config=config)
         fit_model = False
 
     active_learn_alg_ctor = make_libact_strategy_ctor(lambda tr_ds: strat(
-        tr_ds, active_tagger), max_samples_number=max_samples_number)
+            tr_ds, active_tagger), max_samples_number=config.al.max_samples_number)
 
     active_learner = ActiveLearner(active_learn_alg_ctor=active_learn_alg_ctor,
-                                   y_dtype='str',
-                                   X_full_dataset=X_train,
-                                   y_full_dataset=y_seed,
-                                   X_test_dataset=X_test,
-                                   y_test_dataset=y_test,
-                                   model_evaluate=active_tagger,
-                                   eval_metrics=[f1_score],
-                                   rnd_start_steps=0)
-    print(models_path)
+                                       y_dtype='str',
+                                       X_full_dataset=X_train,
+                                       y_full_dataset=y_seed,
+                                       X_test_dataset=X_test,
+                                       y_test_dataset=y_test,
+                                       model_evaluate=active_tagger,
+                                       eval_metrics=[f1_score],
+                                       rnd_start_steps=0)
+
     statistics = emulate_active_learning(y_train, active_learner,
-                                         max_iterations=config.al.n_iterations,
-                                         fit_model=fit_model)
+                                             max_iterations=config.al.n_iterations,
+                                             fit_model=fit_model)
     dump_file(statistics, models_path, f'statistics{repeat}.json')
+        
 
 
 @hydra.main(config_path=os.environ['HYDRA_CONFIG_PATH'])
@@ -415,6 +420,7 @@ def main(config):
     processes = []
 
     print(f'Overall time for {config.n_repeats} repeats took {time.time() - starttime} seconds')
+
 
 
 if __name__ == "__main__":
